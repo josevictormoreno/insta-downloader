9ebd4e08796aeaeb6f241314b65129fcc2d3afdf
josevictormoreno <josevictor.admoreno@gmail.com>
2022-06-28 10:04:32 -0300

    feat: download profile pic

--- main.py
+++ main.py
@@ -2,3 +2,10 @@
 
 ## Author: José Victor Moreno
 ## Github: https://github.com/josevictormoreno
+
+import instaloader 
+
+instagram = instaloader.Instaloader()
+user_profile = input('Enter the instagram id: ')
+
+instagram.download_profile(user_profile, profile_pic_only=True)
